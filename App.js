ed6ea87a30d805e3393682728404584598b95bf1
Franco Fabian <francotirador1785@gmail.com>
2026-09-10 21:43:39 -0300

    BDD y pantalla ADMIN mesas

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import TablesManagerScreen from './src/features/admin/screens/TablesManagerScreen.jsx';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <TablesManagerScreen />
     </View>
   );
 }
@@ -13,8 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#3B1F1B',
   },
 });
